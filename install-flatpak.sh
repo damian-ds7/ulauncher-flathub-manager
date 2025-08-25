47138a8a0f401029715eef839463201c3c72ce0d
Damian D'Souza <dsouzadamian189@gmail.com>
2025-08-25 20:23:03 +0200

    add notification for when installation fails

--- install-flatpak.sh
+++ install-flatpak.sh
@@ -4,13 +4,17 @@ SCRIPT_PATH="$(dirname "$0")"
 APP_ID=$1
 APP_NAME=$2
 
-flatpak install flathub -y "$APP_ID" >/dev/null
+if flatpak install flathub -y "$APP_ID" >/dev/null; then
+    choice=$(notify-send -a "Flathub Search" \
+      -i "$SCRIPT_PATH/images/icon.png" \
+      "$APP_NAME was installed successfully" \
+      -A launch=Launch)
 
-choice=$(notify-send -a "Flathub Search" \
-  -i "$SCRIPT_PATH/images/icon.png" \
-  "$APP_NAME was installed successfully" \
-  -A launch=Launch)
-
-if [[ "$choice" == "launch" ]]; then
-  flatpak run "$APP_ID" >/dev/null
+    if [[ "$choice" == "launch" ]]; then
+        flatpak run "$APP_ID" >/dev/null
+    fi
+else
+    notify-send -a "Flathub Search" \
+      -i "$SCRIPT_PATH/images/icon.png" \
+      "Failed to install $APP_NAME"
 fi
